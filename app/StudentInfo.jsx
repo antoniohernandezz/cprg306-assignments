f77be3a111a95a3eb3e7c43ec0bc9f6a74835b72
antoniohernandezz <antoniohernandezz.ca@gmail.com>
2023-09-14 17:00:40 -0600

    Week 2 Assignment
    - Added React component
    - Added Student Info
    - Added Week 2 page

---
+++ app/StudentInfo.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function MyInfo(){
+    return(
+        <div>
+            <p> Antonio Hernandez </p>
+            <p> CPRG-306 B </p>
+            <Link href='https://github.com/antoniohernandezz'> Github Username</Link>
+        </div>
+    );
+}
---
+++ app/week2/page.js
@@ -0,0 +1,12 @@
+import StudentInfo from '../StudentInfo'
+
+
+export default function ShoppingList() {
+    return (
+        <div>
+            <h1>ShoppingList</h1>
+            <StudentInfo/>
+        </div>
+
+    );
+}
