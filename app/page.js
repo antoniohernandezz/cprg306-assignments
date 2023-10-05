fe9512cc1964bc3224b54d152eef126989624762
antoniohernandezz <antoniohernandezz.ca@gmail.com>
2023-10-05 17:05:43 -0600

    Week 5
    In app\week5 I have added the following:
    
    item-list.js
    item.js
    items.json
    page.js

--- app/page.js
+++ app/page.js
@@ -17,7 +17,7 @@ export default function Home() {
       <Link href="/week2">Week 2</Link> 
       <Link href="/week3">Week 3</Link>
       <Link href="/week4">Week 4</Link>
-
+      <Link href="/week5">Week 5</Link>
     </main>
   );
 }
---
+++ app/week5/page.js
@@ -0,0 +1,11 @@
+import item from "./item";
+import Sorting from "./item-list";
+
+export default function Page(){
+    return(
+        <main className="flex flex-col items-center justify-between">
+            <h1></h1>
+            <Sorting/>
+        </main>
+    );
+}
