7ead8e30c857d3a0e643e93a88a2d1e705117871
antoniohernandezz <antoniohernandezz.ca@gmail.com>
2023-10-23 22:04:40 -0600

    week 6
    Added Folder week 6
    constructed interactive component to add a item to shopping list.
    Added Item-list.js, item.js, items.json, new-item.js and page.js all under week 6 folder.

--- app/page.js
+++ app/page.js
@@ -18,6 +18,8 @@ export default function Home() {
       <Link href="/week3">Week 3</Link>
       <Link href="/week4">Week 4</Link>
       <Link href="/week5">Week 5</Link>
+      <Link href="/week6">Week 6</Link>
+      <Link href="/week7">Week 7</Link>
     </main>
   );
 }
